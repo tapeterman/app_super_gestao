f959346fde373adb5195369146c7db2a87b2ab56
tapeterman <tapetermann@outlook.com.br>
2022-01-22 00:32:05 -0300

    Redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ Route::get('/', function () {
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class , 'principal'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class,'principal'])->name('site.contato');
-
 Route::get('/login', function(){return "Login";})->name('site.login');
 
 
@@ -33,3 +32,8 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function(){return "produtos";})->name('app.produtos');
 });
 
+Route::get('/rota1', function(){return "rota1";})->name('app.rota1');
+Route::get('/rota2', function(){return redirect()->route('app.rota1');}
+)->name('app.rota2');
+
+//Route::redirect('/rota2','/rota1');
