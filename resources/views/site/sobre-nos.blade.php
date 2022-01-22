e062133d5e58033b08fb8ee8e7aa47657526761d
tapeterman <tapetermann@outlook.com.br>
2022-01-21 23:36:53 -0300

    criacao das rotas principal,contato e sobre-nos

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,1 @@
+<h3>sobre nos (view)</h3>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{PrincipalController,SobreNosController,ContatoController};
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return "ola seja bem vindo";
 });
+*/
+
+Route::get('/'          ,    [PrincipalController::class , 'principal']);
+Route::get('/sobre-nos' ,    [SobreNosController::class  , 'principal']);
+Route::get('/contato'   ,    [ContatoController::class   , 'principal']);
+
+//Route::get($uri,$callback)
+
+
+
