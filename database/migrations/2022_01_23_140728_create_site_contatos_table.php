4de286f2ac61088684043be0b024254c6fec5e7f
tapeterman <tapetermann@outlook.com.br>
2022-01-23 12:26:25 -0300

    add migrations e conexao com mysql

---
+++ database/migrations/2022_01_23_140728_create_site_contatos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 100);
+            $table->string('telefone', 100);
+            $table->string('email', 100);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_contatos');
+    }
+}
