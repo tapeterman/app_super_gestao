5144af5c4db32772e241ec2d2a7cb9b823e7f01e
tapeterman <tapetermann@outlook.com.br>
2022-01-24 21:09:36 -0300

    criando migrations e relacionamentos

--- database/migrations/2022_01_24_220012_alter_forncedores_novas_colunas.php
+++ database/migrations/2022_01_24_220012_alter_forncedores_novas_colunas.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFornecedorTable extends Migration
+class AlterForncedoresNovasColunas extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateFornecedorTable extends Migration
      */
     public function up()
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome',50);
-            $table->timestamps();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf','2');
+            $table->string('email','150');
             
         });
     }
@@ -28,6 +27,10 @@ class CreateFornecedorTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            //$table->dropColumn('uf');
+            $table->dropColumn(['uf','email']);
+            
+        });
     }
 }
---
+++ database/migrations/2022_01_24_233024_create_unidades_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade',5); //cm,mm,kg,grama
+            $table->string('descricao',30);
+            $table->timestamps();
+        });
+
+        //add relacionamento com table produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        //add relacionamento com table produto_detalhes
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //remover relacionamento com table produto_detalhes
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->dropForeign('produto_detalhes_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+        //add relacionamento com table produtos
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::dropIfExists('unidades');
+
+    }
+}
