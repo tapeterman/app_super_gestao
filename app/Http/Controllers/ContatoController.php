255ff0e4d0812bd96e52b69b9593147fbfa9951c
tapeterman <tapetermann@outlook.com.br>
2022-01-26 21:52:20 -0300

    salvando dados do formulario com metodo create

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function principal(){
-        //var_dump($_POST);
-        return view('site.contato');
+    public function principal(Request $request){
+        
+        /*metodo
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        
+        //metodo fill
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        */
+         //metodo create
+         $contato = new SiteContato();
+         $contato->create($request->all());
+
+        //print_r($request->All());
+        return view('site.contato',['titulo' => 'Contato (teste)']);
     }
 }
 
